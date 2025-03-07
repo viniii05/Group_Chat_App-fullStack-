dba2a531f4d77f7f8daba18ad656263206071931
viniii05 <vinivt.0520@gmail.com>
2025-03-07 17:59:10 +0530

    created login api and jwt authentication

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,12 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/UserData');
 const path = require('path');
 const rootDir = require('../util/path');
+
+require('dotenv').config();
+
+const SECRET_KEY = process.env.JWT_SECRET;
 
 exports.getSignupForm = (req,res) => {
     res.sendFile(path.join(rootDir,'views','signup.html'));
@@ -30,4 +35,26 @@ exports.postSignupData = async (req, res) => {
     }
 };
 
+exports.postLoginData = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ where: { email } });
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({ error: "User not authorized" });
+        }
+
+        const token = jwt.sign({ id: user.id }, SECRET_KEY, { expiresIn: "1h" });
+
+        res.status(200).json({ message: "Login successful", token });
+    } catch (error) {
+        console.error("Login error:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,7 @@ const userController = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/signup', userController.postSignupData);
+router.post('/login', userController.postLoginData);
 
 router.get('/user/login', userController.getLoginForm);
 router.get('/user/signup', userController.getSignupForm);
